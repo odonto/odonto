fa40e84cb3ce205f7876f4881fff816dec7065c1
Chris Lamb <chris@chris-lamb.co.uk>
2018-02-12 15:28:24 +0000

    Let's try and pretend to be a Django form.

--- fp17/message.py
+++ fp17/message.py
@@ -3,6 +3,22 @@ import xmlschema
 
 
 class Message(object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._errors = None
+
+    def full_clean(self):
+        self._errors = []
+
+    @property
+    def errors(self):
+        if self._errors is None:
+            self.full_clean()
+        return self._errors
+
+    def is_valid(self):
+        return not bool(self._errors)
+
     @classmethod
     def validate_xml(cls, root):
         schema = xmlschema.XMLSchema(os.path.join('xsd', cls.Meta.schema))
