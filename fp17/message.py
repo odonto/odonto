073f01ecfb30855fdd8e0b49c8e7a433ce856222
Chris Lamb <chris@chris-lamb.co.uk>
2018-02-12 15:37:01 +0000

    Demo some kind of error checking

--- fp17/message.py
+++ fp17/message.py
@@ -5,19 +5,23 @@ import xmlschema
 class Message(object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._errors = None
 
-    def full_clean(self):
-        self._errors = []
+        self._errors = {}  # (str, None) -> [str]
+
+    def add_error(self, field, error):
+        self._errors.setdefault(field, []).extend(error)
 
     @property
     def errors(self):
-        if self._errors is None:
-            self.full_clean()
+        if not self._errors:
+            self.clean()
         return self._errors
 
+    def full_clean(self):
+        self._errors = {}
+
     def is_valid(self):
-        return not bool(self._errors)
+        return not bool(self.errors)
 
     @classmethod
     def validate_xml(cls, root):
--- tests/test_bcds1.py
+++ tests/test_bcds1.py
@@ -3,6 +3,6 @@ from fp17.bcds1 import BCDS1Message
 
 def test_simple():
     msg = BCDS1Message()
-    assert msg.is_valid()
+    assert not msg.is_valid()
     root = msg.generate_xml()
     BCDS1Message.validate_xml(root)
