c9fec3f5bae5338ca9cdd8f45d91e84523635bc0
Marcus Baw <marcusbaw@gmail.com>
2018-03-02 15:50:11 +0000

    created DentalCareEpisodeCategory

--- odonto/__init__.py
+++ odonto/__init__.py
@@ -2,9 +2,13 @@
 odonto - Our Opal Application
 """
 from opal.core import application, menus
+from odonto import episode_categories
+
 
 class Application(application.OpalApplication):
-    javascripts   = [
+    default_episode_category = episode_categories.DentalCareEpisodeCategory.display_name
+
+    javascripts  = [
         'js/openodonto/routes.js',
         'js/opal/controllers/discharge.js',
         # Uncomment this if you want to implement custom dynamic flows.
---
+++ odonto/episode_categories.py
@@ -0,0 +1,6 @@
+from opal.core import episodes
+
+
+class DentalCareEpisodeCategory(episodes.EpisodeCategory):
+    display_name = "Dental Care"
+    detail_template = "detail/dental_care.html"
