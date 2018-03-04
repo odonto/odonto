0d675e5cd47f0d1a70c273a72c61723469f92259
David Miller <david@deadpansincerity.com>
2018-03-04 22:13:30 +0000

    Undo mistaken create episode view

--- odonto/urls.py
+++ odonto/urls.py
@@ -8,12 +8,8 @@ from opal.urls import urlpatterns as opatterns
 from django.contrib import admin
 admin.autodiscover()
 
-from odonto import views
 
 urlpatterns = [
-    url(r'^episode/for/new/fp17/(?P<pk>\d+)?',
-        views.CreateNewEpidsodeForFP17View.as_view(),
-        name='episode_for_fp17'),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
