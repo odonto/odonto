a7fda7096d327ec67b769a3383fbd78718637011
David Miller <david@deadpansincerity.com>
2018-03-04 19:57:16 +0000

    Add same_as_acceptance date filler button

--- odonto/static/js/openodonto/controllers/fp17treatment.step.controller.js
+++ odonto/static/js/openodonto/controllers/fp17treatment.step.controller.js
@@ -7,5 +7,9 @@ angular.module('opal.controllers').controller('FP17TreatmentStepCtrl', function(
         scope.editing.fp17_incomplete_treatment.date_of_acceptance = new Date();
     }
 
+    scope.completion_same_as_acceptance = function(){
+        var treatment = scope.editing.fp17_incomplete_treatment;
+        treatment.completion_or_last_visit = treatment.date_of_acceptance;
+    }
 
 });
