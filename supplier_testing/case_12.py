97fa48450bd18e042679d0601cc4eedce0fd9326
Chris Lamb <chris@openhealthcare.org.uk>
2018-03-05 09:31:49 +0000

    Complete case 12.

--- supplier_testing/case_12.py
+++ supplier_testing/case_12.py
@@ -24,7 +24,12 @@ if __name__ == '__main__':
     # Fillings x 3, Recall Interval 24, Decayed Deciduous 4, Ethnic Origin 12"
     bcds1.treatments = [
         treatments.TREATMENT_CATEGORY_BAND_2,
-
+        treatments.EXAMINATION,
+        treatments.SCALE_AND_POLISH,
+        treatments.RADIOGRAPHS(1),
+        treatments.PERMANENT_FILLINGS_AND_SEALANT_RESTORATIONS(3),
+        treatments.RECALL_INTERVAL(24),
+        treatments.DECAYED_DECIDUOUS(4),
         treatments.ETHNIC_ORIGIN_12_BLACK_OR_BLACK_BRITISH_CARIBBEAN,
     ]
 
