0054bd223acc0535c5f4a99331d5844268afe361
Chris Lamb <chris@openhealthcare.org.uk>
2018-03-05 09:57:01 +0000

    Drop random punctuation.

--- supplier_testing/case_23.py
+++ supplier_testing/case_23.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         treatments.TREATMENT_CATEGORY_BAND_2,
         treatments.SCALE_AND_POLISH,
         treatments.RADIOGRAPHS(1),
-        treatments.PERMANENT_FILLINGS_AND_SEALANT_RESTORATIONS),
+        treatments.PERMANENT_FILLINGS_AND_SEALANT_RESTORATIONS,
         treatments.REFERRAL_FOR_ADVANCED_MANDATORY_SERVICES_LEGACY,
         treatments.ETHNIC_ORIGIN_6_WHITE_AND_ASIAN,
     ]
