a50eeacb81da6b5261d99aca1701f7bdf7abe43f
Chris Lamb <chris@openhealthcare.org.uk>
2018-03-05 09:33:34 +0000

    Complete case 14.

--- supplier_testing/case_14.py
+++ supplier_testing/case_14.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     # Treatments: "Fluoride Varnish, Filling x 1, Ethnic Origin 14"
     bcds1.treatments = [
         treatments.TREATMENT_CATEGORY_BAND_2,
-
+        treatments.PERMANENT_FILLINGS_AND_SEALANT_RESTORATIONS(1),
         treatments.ETHNIC_ORIGIN_14_OTHER_BLACK_BACKGROUND,
     ]
 
