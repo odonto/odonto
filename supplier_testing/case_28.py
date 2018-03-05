695015696857a6352ffc7aae7bc3291934603d0b
Chris Lamb <chris@openhealthcare.org.uk>
2018-03-05 10:59:13 +0000

    Complete case 28.

--- supplier_testing/case_28.py
+++ supplier_testing/case_28.py
@@ -22,7 +22,8 @@ if __name__ == '__main__':
     # Treatments: "Fillings x 1, Recall Interval (9172 12), Ethnic Origin 11"
     bcds1.treatments = [
         treatments.TREATMENT_CATEGORY_BAND_2,
-
+        treatments.PERMANENT_FILLINGS_AND_SEALANT_RESTORATIONS(1),
+        treatments.RECALL_INTERVAL(12),
         treatments.ETHNIC_ORIGIN_11_OTHER_ASIAN_BACKGROUND,
     ]
 
