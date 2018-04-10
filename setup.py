f626bb604cafcf426f48212d9cac77a6066a648e
Chris Lamb <chris@openhealthcare.org.uk>
2018-04-10 11:13:39 +0100

    Add dependencies to setup.py. (Closes: #6)

--- setup.py
+++ setup.py
@@ -15,4 +15,12 @@ setup(
 
     packages=find_packages(),
     include_package_data=True,
+
+    install_requires=(
+        'cerberus',
+        'click',
+        'lxml',
+        'requests',
+        'xmlschema',
+    ),
 )
