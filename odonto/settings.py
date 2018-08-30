bb82010ec05f557b19108328349aad35c3a03314
Chris Lamb <chris@openhealthcare.org.uk>
2018-08-30 10:45:05 +0100

    Add some trailing commas.

--- odonto/settings.py
+++ odonto/settings.py
@@ -26,7 +26,7 @@ except ImportError:
             'USER': '',
             'PASSWORD': '',
             'HOST': '',
-            'PORT': ''
+            'PORT': '',
         }
     }
 
@@ -153,7 +153,6 @@ TEMPLATES = [
 
             ],
 
-
             # ... some options here ...
         },
     },
@@ -191,14 +190,14 @@ LOGGING = {
     'disable_existing_loggers': False,
     'filters': {
         'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
+            '()': 'django.utils.log.RequireDebugFalse',
         }
     },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
         }
     },
     'loggers': {
