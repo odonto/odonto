34f25406fbeaf60265d9845330fd082ed6f898d1
fredkingham <fredkingham@gmail.com>
2019-04-29 11:25:11 +0100

    Adds a number field and a teeth field that contain have min/max properties and validate accordingly

--- odontotheme/templatetags/odonto_forms.py
+++ odontotheme/templatetags/odonto_forms.py
@@ -27,3 +27,32 @@ def btn_checkbox(*args, **kwargs):
 @register.inclusion_tag('_helpers/btn_radio.html')
 def btn_radio(*args, **kwargs):
     return extract_common_args(kwargs)
+
+
+def extract_numeric_args(kwargs):
+    ctx = extract_common_args(kwargs)
+    numeric_args = [
+        "min", "max"
+    ]
+
+    for i in numeric_args:
+        if i in kwargs:
+            ctx[i] = kwargs.pop(i)
+    return ctx
+
+
+@register.inclusion_tag('_helpers/number.html')
+def number(*args, **kwargs):
+    ctx = extract_numeric_args(kwargs)
+    ctx["style"] = "vertical"
+    return ctx
+
+@register.inclusion_tag('_helpers/number.html')
+def teeth(*args, **kwargs):
+    ctx = extract_numeric_args(kwargs)
+    ctx.setdefault("min", 0)
+    ctx.setdefault("max", 50)
+    ctx["style"] = "vertical"
+    return ctx
+
+
