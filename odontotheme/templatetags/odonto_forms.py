dc8e52759b00daa2113b1576b012512b0ad0fb0b
fredkingham <fredkingham@gmail.com>
2019-06-14 11:49:12 +0100

    Change the char field and datepicker fields to display errors if an error object on scope is populated. The error object is populated with model -> field_name -> error_msg or model -> 'step_base' -> error_msg

--- odontotheme/templatetags/odonto_forms.py
+++ odontotheme/templatetags/odonto_forms.py
@@ -2,9 +2,28 @@
 Templatetags for form/modal helpers
 """
 from django import template
-from opal.templatetags.forms import extract_common_args, _input
+from opal.templatetags.forms import (
+    extract_common_args, _input, _model_and_field_from_path
+)
 
 register = template.Library()
+
+
+def get_odonto_common_args(kwargs):
+    ctx = extract_common_args(kwargs)
+    api_name, field_name = get_field_name_and_api_name(kwargs["field"])
+    ctx["model_api_name"] = api_name
+    ctx["field_name"] = field_name
+    return ctx
+
+
+def get_field_name_and_api_name(subrecord_field_path):
+    """
+    returns the field and the api name for the context
+    """
+    _, field_name = subrecord_field_path.split('.')
+    model, field = _model_and_field_from_path(subrecord_field_path)
+    return model.get_api_name(), field_name
 
 
 @register.inclusion_tag('_helpers/btn_checkbox.html')
@@ -20,17 +39,17 @@ def btn_checkbox(*args, **kwargs):
     - lookuplist: Name of the lookuplist
     - required: label to show when we're required!
     """
-    return extract_common_args(kwargs)
+    return get_odonto_common_args(kwargs)
 
 
 
 @register.inclusion_tag('_helpers/btn_radio.html')
 def btn_radio(*args, **kwargs):
-    return extract_common_args(kwargs)
+    return get_odonto_common_args(kwargs)
 
 
 def extract_numeric_args(kwargs):
-    ctx = extract_common_args(kwargs)
+    ctx = get_odonto_common_args(kwargs)
     numeric_args = [
         "min", "max"
     ]
@@ -65,6 +84,9 @@ def charfield(*args, **kwargs):
     """
     ctx = _input(*args, **kwargs)
     ctx["style"] = "vertical"
+    api_name, field_name = get_field_name_and_api_name(kwargs["field"])
+    ctx["model_api_name"] = api_name
+    ctx["field_name"] = field_name
 
     if "pattern" not in kwargs and "pattern_error" in kwargs:
         raise ValueError("Pattern error passed in but no pattern")
@@ -73,4 +95,17 @@ def charfield(*args, **kwargs):
         if field in kwargs:
             ctx[field] = kwargs[field]
     return ctx
+
+
+@register.inclusion_tag('_helpers/datepicker.html')
+def odonto_datepicker(*args, **kwargs):
+    kwargs["datepicker"] = True
+    context = _input(*args, **kwargs)
+    api_name, field_name = get_field_name_and_api_name(kwargs["field"])
+    context["model_api_name"] = api_name
+    context["field_name"] = field_name
+    if 'mindate' in kwargs:
+        context['mindate'] = kwargs['mindate']
+    context["user_options"] = kwargs.pop("user_options", False)
+    return context
 
