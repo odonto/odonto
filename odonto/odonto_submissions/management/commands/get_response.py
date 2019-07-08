af90cca84e91aa1ec59b4903595fee641b5746ac
fredkingham <fredkingham@gmail.com>
2019-07-08 12:43:59 +0100

    removes a now unneeded send message command and adds management commands that you can now pass an episode id

--- odonto/odonto_submissions/management/commands/get_response.py
+++ odonto/odonto_submissions/management/commands/get_response.py
@@ -5,9 +5,7 @@ from django.core.management.base import BaseCommand
 from odonto.odonto_submissions import dpb_api
 
 
-
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
         response = dpb_api.get_responses()
         print(response.content)
