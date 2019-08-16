ed0897b1b4e4ed7be57b12666bc93fd409b7e5f7
fredkingham <fredkingham@gmail.com>
2019-08-16 10:47:38 +0100

    Allow free treatments, although we need to more feed back from dentists, this is _less_ wrong

--- odonto/static/js/openodonto/services/validators/exemptions_and_remissions.js
+++ odonto/static/js/openodonto/services/validators/exemptions_and_remissions.js
@@ -24,6 +24,14 @@ angular.module('opal.services').factory('ExemptionsAndRemissionsValidator', func
     "evidence_of_exception_or_remission_seen",
   ]
 
+  var FREE_TREATMENTS = [
+    "prescription_only",
+    "denture_repairs",
+    "bridge_repairs",
+    "arrest_of_bleeding",
+    "removal_of_sutures"
+  ]
+
   var exceptionOrCharge = function(editing){
     if(!editing.fp17_exemptions.patient_charge_collected){
       var exemptionSelected = _.some(EXEMPTION_FIELDS, function(exemptionField){
@@ -36,8 +44,22 @@ angular.module('opal.services').factory('ExemptionsAndRemissionsValidator', func
     }
   }
 
+
+  /*
+  These treatments don't require payment. This may not be _exactly_ correct
+  as its plausible that the patient has other treatments _aswell_ as these
+  however we are going back to the dentists for clarification.
+  */
+  var freeTreatments = function(editing){
+    var result = _.find(FREE_TREATMENTS, function(ft){
+      return editing.fp17_treatment_category[ft];
+    });
+
+    return result;
+  }
+
   return function(editing) {
-    if(exceptionOrCharge(editing)){
+    if(exceptionOrCharge(editing) && !freeTreatments(editing)){
       return {
         fp17_exemptions: {
           step_error: "Please select an exemption or add the charge"
