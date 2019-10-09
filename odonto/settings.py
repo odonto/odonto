eae346ea5f7d31cafad2dfede6fc11b9643aa893
fredkingham <fredkingham@gmail.com>
2019-10-09 16:09:37 +0100

    version bump

--- odonto/settings.py
+++ odonto/settings.py
@@ -307,7 +307,7 @@ DEFAULT_DOMAIN = 'http://openodonto.com/'
 # Begins OPAL Settings you should edit !
 
 OPAL_BRAND_NAME = 'Open Odonto FP17'
-VERSION_NUMBER = '0.10.0'
+VERSION_NUMBER = '0.11.0'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
