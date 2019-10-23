f4cfc209a806bcfc46e19dc1ebe57f16e9e54c63
fredkingham <fredkingham@gmail.com>
2019-10-23 13:05:48 +0100

    Adds front end validation to make sure that date of appliance is never less than date of assessment

--- odonto/__init__.py
+++ odonto/__init__.py
@@ -22,4 +22,5 @@ class Application(application.OpalApplication):
         'js/openodonto/services/validators/form_validation.js',
         'js/openodonto/services/validators/date_of_birth_required.js',
         'js/openodonto/services/validators/address_required.js',
+        'js/openodonto/services/validators/appliance_greater_than_assessment.js',
     ]
