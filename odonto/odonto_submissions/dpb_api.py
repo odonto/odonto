17df6ca7ca666f7a8784b811f8062f03e2eb9f08
fredkingham <fredkingham@gmail.com>
2019-11-13 19:27:24 +0000

    write responses to different files each day, also to the logs and make sure that we check the setting to see if we can get responses

--- odonto/odonto_submissions/dpb_api.py
+++ odonto/odonto_submissions/dpb_api.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import datetime
 from django.conf import settings
 from requests.auth import HTTPBasicAuth
 from .exceptions import MessageSendingException
@@ -10,36 +12,24 @@ RESPONSES_URL = "https://ebusiness.dpb.nhs.uk/responses.asp"
 
 
 def send_message(xml):
-    logger.info(
-        "send_message() called with {}".format(
-            xml
-        )
-    )
+    logger.info("send_message() called with {}".format(xml))
     if settings.SEND_MESSAGES:
         request_kwargs = {
             "auth": HTTPBasicAuth(settings.DPB_USERNAME, settings.DPB_PASSWORD),
-            "data":  xml
+            "data": xml,
         }
         if hasattr(settings, "PROXY"):
             request_kwargs["proxies"] = settings.PROXY
 
-        result = requests.post(
-            CLAIMS_URL, **request_kwargs
-        )
-        logger.info(
-            "result {} {}".format(
-                result.status_code, result.content
-            )
-        )
+        result = requests.post(CLAIMS_URL, **request_kwargs)
+        logger.info("result {} {}".format(result.status_code, result.content))
         if result.status_code != 200:
             err = "Message sending resulted in {result.status_code} and \
 {result.content}"
             raise MessageSendingException(err)
         else:
             response = result.content
-            logger.info(
-                "message sent, received a response of {}".format(response)
-            )
+            logger.info("message sent, received a response of {}".format(response))
             return response
     else:
         logger.info("NOT SENDING MESSAGE BECAUSE SEND_MESSAGES=False")
@@ -47,19 +37,35 @@ def send_message(xml):
 
 
 def get_responses():
+    logger.info("getting responses")
+    responses_dir = os.path.join(f"{settings.PROJECT_PATH}", "..", "responses")
+    if not os.path.exists(responses_dir):
+        raise ValueError(
+            f"Unable to get responses as the save dir {responses_dir} does not exist"
+        )
     request_kwargs = {
-        "auth": HTTPBasicAuth(settings.DPB_USERNAME, settings.DPB_PASSWORD),
+        "auth": HTTPBasicAuth(settings.DPB_USERNAME, settings.DPB_PASSWORD)
     }
     if hasattr(settings, "PROXY"):
         request_kwargs["proxies"] = settings.PROXY
-    response = requests.get(
-        RESPONSES_URL,
-        **request_kwargs
-    )
+
+    if not settings.SEND_MESSAGES:
+        logger.info("NOT REQUESTING RESPONSES BECAUSE SEND_MESSAGES=False")
+        return "SEND_MESSAGES=False: responses not requested"
+
+    response = requests.get(RESPONSES_URL, **request_kwargs)
 
     if response.ok:
+        logger.info(f"Responses recieved {response.text}")
+        dt = datetime.datetime.now().strftime("%d-%m-%y-%H-%M")
+        file_name = f"responses-{dt}.xml"
+
+        with open(os.path.join(responses_dir, file_name), "w") as r:
+            r.write(response.text)
         return response.text
 
+    logger.info(f"Response is not ok, with '{response.text}'")
     raise ValueError(
-        f'Unable to get responses {response.status_code} {response.content}'
+        f"Unable to get responses {response.status_code} {response.content}"
     )
+
---
+++ odonto/odonto_submissions/tests/test_dpb_api.py
@@ -0,0 +1,56 @@
+import datetime
+from importlib import reload
+from django.test import override_settings
+from unittest import mock
+from opal.core.test import OpalTestCase
+from odonto.odonto_submissions import dpb_api
+
+
+@mock.patch("odonto.odonto_submissions.dpb_api.datetime")
+@mock.patch("odonto.odonto_submissions.dpb_api.requests.get")
+@mock.patch("odonto.odonto_submissions.dpb_api.os.path.exists")
+@mock.patch("odonto.odonto_submissions.dpb_api.logger")
+class DpbApiTestCase(OpalTestCase):
+    @override_settings(SEND_MESSAGES=True, PROJECT_PATH="my_project")
+    def test_get_responses(self, logger, os_exits, get, dt):
+        dt.datetime.now.return_value = datetime.datetime(2019, 11, 10, 9, 8)
+        with mock.patch(
+            "odonto.odonto_submissions.dpb_api.open", mock.mock_open(), create=True
+        ) as m:
+            get.return_value.text = "job done"
+            result = dpb_api.get_responses()
+            self.assertEqual(result, "job done")
+            expected_file_name = "my_project/../responses/responses-10-11-19-09-08.xml"
+            m.assert_called_once_with(expected_file_name, "w")
+            m.return_value.write.assert_called_once_with("job done")
+
+    @override_settings(SEND_MESSAGES=False, PROJECT_PATH="my_project")
+    def test_send_messages_false(self, logger, os_exits, get, dt):
+        dt.datetime.now.return_value = datetime.datetime(2019, 11, 10, 9, 8)
+        with mock.patch(
+            "odonto.odonto_submissions.dpb_api.open", mock.mock_open(), create=True
+        ) as m:
+            get.return_value.text = "job done"
+            result = dpb_api.get_responses()
+        self.assertFalse(get.called)
+        self.assertFalse(m.called)
+        self.assertEqual(result, "SEND_MESSAGES=False: responses not requested")
+
+    @override_settings(SEND_MESSAGES=True, PROJECT_PATH="my_project")
+    def test_response_fails(self, logger, os_exits, get, dt):
+        dt.datetime.now.return_value = datetime.datetime(2019, 11, 10, 9, 8)
+        with mock.patch(
+            "odonto.odonto_submissions.dpb_api.open", mock.mock_open(), create=True
+        ):
+            get.return_value.ok = False
+            get.return_value.text = "job failed"
+            with self.assertRaises(ValueError):
+                dpb_api.get_responses()
+
+            self.assertEqual(logger.info.call_args_list[0][0][0], "getting responses")
+
+            self.assertEqual(
+                logger.info.call_args_list[1][0][0],
+                "Response is not ok, with 'job failed'",
+            )
+
