ab93fbc23cee7379263b15a2b3831f61baceae1c
fredkingham <fredkingham@gmail.com>
2019-11-20 11:49:46 +0000

    Removes the Oldest FP17 admin filter as we are now sounding these down and getting a daily report

--- odonto/odonto_submissions/admin.py
+++ odonto/odonto_submissions/admin.py
@@ -1,83 +1,10 @@
-import datetime
 from django.contrib import admin
 from opal import models as opal_models
-from odonto import models as odonto_models
 from . import models
 from reversion.admin import VersionAdmin
 from odonto import episode_categories
 from dateutil.relativedelta import relativedelta
 from django.utils.html import format_html
-
-
-class OldestFP17(admin.SimpleListFilter):
-    title = "Old FP17s"
-    NEARLY_THREE_MONTHS = 'nearly_3_months'
-    OVER_THREE_MONTHS = 'over_3_months'
-    NEARLY_TWO_MONTHS = 'nearly_2_months'
-    OVER_TWO_MONTHS = 'over_2_months'
-
-    parameter_name = "deadline"
-
-    def lookups(self, request, model_admin):
-        return (
-            (self.NEARLY_THREE_MONTHS, 'Nearly 3 months'),
-            (self.OVER_THREE_MONTHS, 'Over 3 month'),
-            (self.NEARLY_TWO_MONTHS, 'Nearly 2 months'),
-            (self.OVER_TWO_MONTHS, 'Oever 2 months'),
-        )
-
-    def queryset(self, request, queryset):
-        """
-        Returns the filtered queryset based on the value
-        provided in the query string and retrievable via
-        `self.value()`.
-        """
-        treatments = odonto_models.Fp17IncompleteTreatment.objects.filter(
-            episode__stage="Submitted"
-        )
-        treatments = treatments.filter(
-            episode__category_name=episode_categories.FP17Episode.display_name
-        )
-        treatments = treatments.exclude(completion_or_last_visit=None)
-        three_mo = datetime.date.today() - relativedelta(
-            months=3
-        )
-        two_mo = datetime.date.today() - relativedelta(
-            months=2
-        )
-        if self.value() == self.OVER_THREE_MONTHS:
-            treatments = treatments.filter(
-                completion_or_last_visit__lte=three_mo
-            )
-            return queryset.filter(
-                id__in=treatments.values_list('episode_id', flat=True)
-            )
-        if self.value() == self.NEARLY_THREE_MONTHS:
-            treatments = treatments.exclude(
-                completion_or_last_visit__lte=three_mo
-            ).filter(
-                completion_or_last_visit__lte=three_mo + datetime.timedelta(7)
-            )
-            return queryset.filter(
-                id__in=treatments.values_list('episode_id', flat=True)
-            )
-
-        if self.value() == self.OVER_TWO_MONTHS:
-            treatments = treatments.filter(
-                completion_or_last_visit__lte=two_mo
-            )
-            return queryset.filter(
-                id__in=treatments.values_list('episode_id', flat=True)
-            )
-        if self.value() == self.NEARLY_TWO_MONTHS:
-            treatments = treatments.exclude(
-                completion_or_last_visit__lte=two_mo
-            ).filter(
-                completion_or_last_visit__lte=two_mo + datetime.timedelta(7)
-            )
-            return queryset.filter(
-                id__in=treatments.values_list('episode_id', flat=True)
-            )
 
 
 class EpisodeAdmin(VersionAdmin):
@@ -86,8 +13,6 @@ class EpisodeAdmin(VersionAdmin):
         'last_submission_state', 'last_submission_created',
         'summary'
     )
-
-    list_filter = (OldestFP17,)
 
     actions = ["submit_episode_to_compass"]
 
