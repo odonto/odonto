67e87fdf3a1923c619f9c143572a41d0afdcf282
fredkingham <fredkingham@gmail.com>
2019-11-20 11:55:49 +0000

    clean up the episode admin removing now unneeded methods and using the category methods to get the submission

--- odonto/odonto_submissions/admin.py
+++ odonto/odonto_submissions/admin.py
@@ -3,15 +3,13 @@ from opal import models as opal_models
 from . import models
 from reversion.admin import VersionAdmin
 from odonto import episode_categories
-from dateutil.relativedelta import relativedelta
 from django.utils.html import format_html
 
 
 class EpisodeAdmin(VersionAdmin):
     list_display = (
-        'id', 'category_name', 'stage', 'submission_deadline',
-        'last_submission_state', 'last_submission_created',
-        'summary'
+        'id', 'category_name', 'stage',
+        'submission_state', 'summary'
     )
 
     actions = ["submit_episode_to_compass"]
@@ -31,32 +29,16 @@ class EpisodeAdmin(VersionAdmin):
             obj.patient_id, obj.id
         )
 
-    def submission_deadline(self, obj):
-        fp17_name = episode_categories.FP17Episode.display_name
-        if not obj.category_name == fp17_name:
-            return "NA"
-        fp17_incomplete_treatment = obj.fp17incompletetreatment_set.all()[0]
-        last_visit = fp17_incomplete_treatment.completion_or_last_visit
-        if last_visit:
-            return last_visit + relativedelta(months=2)
-
-    def last_submission_state(self, obj):
-        fp17_name = episode_categories.FP17Episode.display_name
-        if not obj.category_name == fp17_name:
-            return "NA"
-        if obj.submission_set.all():
-            submission = list(obj.submission_set.all())[0]
+    def submission_state(self, obj):
+        if obj.category.display_name == episode_categories.DentalCareEpisodeCategory.display_name:
+            return
+        submission = obj.category.submission()
+        if submission:
             return submission.state
-
-    def last_submission_created(self, obj):
-        if obj.submission_set.all():
-            submission = list(obj.submission_set.all())[0]
-            return submission.created
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         qs = qs.prefetch_related('submission_set')
-        qs = qs.prefetch_related('fp17incompletetreatment_set')
         return qs
 
 
