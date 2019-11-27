548c17fac7d8c9667820c9fd7b9b8f03d10d54ab
fredkingham <fredkingham@gmail.com>
2019-11-27 17:52:22 +0000

    Adds validation to make sure the users add in the correct date of referral in fp17O claims

---
+++ odonto/static/js/test/fp17oDateOfReferral.test.js
@@ -0,0 +1,75 @@
+describe('Fp17ODateOfReferral', function() {
+  "use strict";
+  var Fp17ODateOfReferral;
+  var editing;
+
+  beforeEach(module('opal.filters'));
+  beforeEach(module('opal.services'));
+
+  beforeEach(function(){
+      inject(function($injector){
+        Fp17ODateOfReferral  = $injector.get('Fp17ODateOfReferral');
+      });
+      editing = {
+        orthodontic_assessment: {}
+      };
+  });
+
+  describe('required date of referral', function(){
+    it('should error if date of referral is not there and assessment is after 1Apr2019', function(){
+      editing.orthodontic_assessment.date_of_referral = undefined;
+      editing.orthodontic_assessment.date_of_assessment = moment('2019-04-01').toDate();
+      var result = Fp17ODateOfReferral(editing);
+      var error = result.orthodontic_assessment.date_of_referral;
+      expect(error).toBe("Date of referral is required when there's a date of assessment");
+    });
+
+    it('should not error if date of assessment is less that 1Apr2019', function(){
+      editing.orthodontic_assessment.date_of_referral = undefined;
+      editing.orthodontic_assessment.date_of_assessment = moment('2019-03-01').toDate();
+      var result = Fp17ODateOfReferral(editing);
+      expect(result).toBeUndefined();
+    });
+
+    it('should not error if date of assessment is not present', function(){
+      editing.orthodontic_assessment.date_of_referral = undefined;
+      editing.orthodontic_assessment.date_of_assessment = undefined;
+      var result = Fp17ODateOfReferral(editing);
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('date of referral must be less than date of assessment', function(){
+    it('should error if the date of assessment is less than the date of referral', function(){
+      editing.orthodontic_assessment.date_of_referral = moment('2019-04-06').toDate();;
+      editing.orthodontic_assessment.date_of_assessment = moment('2019-04-05').toDate();
+      var result = Fp17ODateOfReferral(editing);
+      var error = result.orthodontic_assessment.date_of_referral;
+      expect(error).toBe("Date of referral must be equal or less than the date of assessment");
+    });
+
+    it('should not error if the date of assessment is the same as the date of referral', function(){
+      editing.orthodontic_assessment.date_of_referral = moment('2019-04-05').toDate();;
+      editing.orthodontic_assessment.date_of_assessment = moment('2019-04-05').toDate();
+      var result = Fp17ODateOfReferral(editing);
+      expect(result).toBeUndefined();
+    });
+
+    it('should  not error if the date of referral is greater than the date of assessment', function(){
+      editing.orthodontic_assessment.date_of_referral = moment('2019-04-04').toDate();;
+      editing.orthodontic_assessment.date_of_assessment = moment('2019-04-05').toDate();
+      var result = Fp17ODateOfReferral(editing);
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('referral cannot be in the future', function(){
+    it('should error if date of referral is in the future', function(){
+      editing.orthodontic_assessment.date_of_referral = moment('2300-04-04').toDate();;
+      editing.orthodontic_assessment.date_of_assessment = moment('2300-04-05').toDate();
+      var result = Fp17ODateOfReferral(editing);
+      var error = result.orthodontic_assessment.date_of_referral;
+      expect(error).toBe("Date of referral cannot be in the future");
+    });
+  })
+});
