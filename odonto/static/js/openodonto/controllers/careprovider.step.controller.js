869b5f17dfb01ab46f251c24263be1b1f2f43dac
fredkingham <fredkingham@gmail.com>
2019-12-18 18:11:09 +0000

    Only set the performer if the performer is not already set, remove some fields from the care provider that we no longer use

--- odonto/static/js/openodonto/controllers/careprovider.step.controller.js
+++ odonto/static/js/openodonto/controllers/careprovider.step.controller.js
@@ -1,10 +1,8 @@
-angular.module('opal.controllers').controller(
-  'CareProviderStepCtrl',
-  function(scope, step, episode){
-  scope.editing.fp17_dental_care_provider.provider_name = 'Northumbria HCT';
-  //    scope.editing.fp17_dental_care_provider.provider_location_number = '327836287';
-  scope.editing.fp17_dental_care_provider.provider_address = "Albion Road Clinic\nAlbion Road\nNorth Shields\nNE29 0HG";
-  if(scope.metadata.performer.current_user){
-      scope.editing.fp17_dental_care_provider.performer = scope.metadata.performer.current_user;
+angular.module('opal.controllers').controller('CareProviderStepCtrl', function(scope, step, episode){
+  "use strict";
+  if(!scope.editing.fp17_dental_care_provider.performer){
+    if(scope.metadata.performer.current_user){
+        scope.editing.fp17_dental_care_provider.performer = scope.metadata.performer.current_user;
+    }
   }
 });
