d3246c7b7b55a5c110d054c7a13fa66dfd0cf491
fredkingham <fredkingham@gmail.com>
2019-12-18 17:56:19 +0000

    stop setting fields that we don't use in the care provider pathway step

--- odonto/static/js/openodonto/controllers/careprovider.step.controller.js
+++ odonto/static/js/openodonto/controllers/careprovider.step.controller.js
@@ -1,7 +1,5 @@
 angular.module('opal.controllers').controller('CareProviderStepCtrl', function(scope, step, episode){
   "use strict";
-  scope.editing.fp17_dental_care_provider.provider_name = 'Northumbria HCT';
-  scope.editing.fp17_dental_care_provider.provider_address = "Albion Road Clinic\nAlbion Road\nNorth Shields\nNE29 0HG";
   if(!scope.editing.fp17_dental_care_provider.performer){
     if(scope.metadata.performer.current_user){
         scope.editing.fp17_dental_care_provider.performer = scope.metadata.performer.current_user;
