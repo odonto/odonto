d6d35eb1dcd872837688537df56016583ea773dc
fredkingham <fredkingham@gmail.com>
2019-12-18 10:52:43 +0000

    Adds the performer to the rejection sheet, also adds an extra section if the patient has a date of acceptance but no date of referral.

--- odonto/odonto_submissions/management/commands/get_rejections.py
+++ odonto/odonto_submissions/management/commands/get_rejections.py
@@ -12,29 +12,35 @@ FILE_NAME = os.path.join("..", "rejections.csv")
 
 
 class Command(BaseCommand):
-    def get_rejected_row(self, episode):
+
+    def get_row(self, episode, rejection_reason):
         return {
             "category": episode.category_name,
             "date": episode.category.get_sign_off_date(),
             "location": episode.fp17dentalcareprovider_set.get().provider_location_number,
+            "performer": episode.fp17dentalcareprovider_set.get().performer,
             "submit_link": episode.category.get_submit_link(),
-            "rejection_reason": episode.category.submission().rejection
+            "rejection_reason": rejection_reason
         }
+
+    def get_rejected_row(self, episode):
+        return self.get_row(episode, episode.category.submission().rejection)
+
+    def get_serialize_error_row(self, episode):
+        row = None
+        try:
+            self.serialize_episode(episode)
+        except Exception as e:
+            row = self.get_row(episode, e)
+        return row
 
     def serialize_episode(self, episode):
         serializers.translate_episode_to_xml(episode, 1, 1, 1)
 
-    def get_serialize_error_row(self, episode):
-        try:
-            self.serialize_episode(episode)
-        except Exception as e:
-            return {
-                "category": episode.category_name,
-                "date": episode.category.get_sign_off_date(),
-                "location": episode.fp17dentalcareprovider_set.get().provider_location_number,
-                "submit_link": episode.category.get_submit_link(),
-                "rejection_reason": f"{e.__class__} {e}"
-            }
+    def get_acceptances_without_referrals(self):
+        return FP17OEpisode.get_submitted_episodes().exclude(
+            orthodonticassessment_date_of_assessment=None
+        ).filter(orthodonticassessment_date_of_referral=None)
 
 
     def handle(self, *args, **kwargs):
@@ -43,6 +49,7 @@ class Command(BaseCommand):
                 'date',
                 'category',
                 'location',
+                'performer',
                 'submit_link',
                 'rejection_reason'
             ]
@@ -63,3 +70,8 @@ class Command(BaseCommand):
                 serialize_error_row = self.get_serialize_error_row(episode)
                 if serialize_error_row:
                     writer.writerow(serialize_error_row)
+
+            writer.writerow({})
+
+            for episode in self.get_acceptances_without_referrals():
+                writer.writerow(self.get_row(episode, "Referral date required"))
