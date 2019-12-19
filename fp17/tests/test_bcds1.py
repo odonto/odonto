b874ff15d5700bba22d99e73b831602fada09b62
fredkingham <fredkingham@gmail.com>
2019-12-19 12:25:25 +0000

    enblackens the test file

--- fp17/tests/test_bcds1.py
+++ fp17/tests/test_bcds1.py
@@ -8,43 +8,32 @@ from fp17 import message
 class CreateTreatmentsTestCase(OpalTestCase):
     def setUp(self):
         self.bdcs1 = bcds1.BCDS1()
-        self.root = etree.Element('bcds1')
+        self.root = etree.Element("bcds1")
 
     def test_treatment_with_no_extra_attrs(self):
         treatment = treatments.REGULATION_11_APPLIANCE
-        self.bdcs1.create_treatments(
-            self.root, "tst", message.flatten(
-                [treatment]
-            )
-        )
+        self.bdcs1.create_treatments(self.root, "tst", message.flatten([treatment]))
         self.assertEqual(
             etree.tostring(self.root, encoding=str),
-            '<bcds1><tst><reptrtty trtcd="9162"/></tst></bcds1>'
+            '<bcds1><tst><reptrtty trtcd="9162"/></tst></bcds1>',
         )
 
     def test_treatment_with_instance_count(self):
         treatment = treatments.EXTRACTION(1)
-        self.bdcs1.create_treatments(
-            self.root, "tst", message.flatten(
-                [treatment]
-            )
-        )
+        self.bdcs1.create_treatments(self.root, "tst", message.flatten([treatment]))
         self.assertEqual(
             etree.tostring(self.root, encoding=str),
-            '<bcds1><tst><reptrtty trtcd="9307" noins="01"/></tst></bcds1>'
+            '<bcds1><tst><reptrtty trtcd="9307" noins="01"/></tst></bcds1>',
         )
 
     def test_treatment_with_teeth(self):
         treatment = treatments.ORTHODONTIC_EXTRACTIONS(["14"])
-        self.bdcs1.create_treatments(
-            self.root, "tst", message.flatten(
-                [treatment]
-            )
-        )
+        self.bdcs1.create_treatments(self.root, "tst", message.flatten([treatment]))
         self.assertEqual(
             etree.tostring(self.root, encoding=str),
-            '<bcds1><tst><reptrtty trtcd="9408"><toid>14</toid></reptrtty></tst></bcds1>'
+            '<bcds1><tst><reptrtty trtcd="9408"><toid>14</toid></reptrtty></tst></bcds1>',
         )
+
 
 class TreatmentTestCase(OpalTestCase):
     def test_equality_with_instance_count(self):
