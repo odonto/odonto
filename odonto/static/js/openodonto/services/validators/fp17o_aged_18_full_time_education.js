1d08548c3aa33ea313d2bd7a02bc5b6582a68af8
fredkingham <fredkingham@gmail.com>
2020-01-14 14:05:34 +0000

    Require an fp17o patient to be 18 in order to qualified for aged 18 and in full time education

---
+++ odonto/static/js/openodonto/services/validators/fp17o_aged_18_full_time_education.js
@@ -0,0 +1,33 @@
+
+angular.module('opal.services').factory('Fp17OAged18InFullTimeEducation', function(toMomentFilter){
+
+  /*
+  * An FP17O can have multiple dates so look at them all starting with the earliest
+  * and make sure the patient was 18 at time of treatment
+  */
+  return function(editing){
+    if(!editing.fp17_exemptions.aged_18_in_full_time_education){
+      return;
+    }
+    if(editing.demographics.date_of_birth){
+      var otherDate = editing.orthodontic_assessment.date_of_referral;
+      otherDate = otherDate || editing.orthodontic_assessment.date_of_assessment;
+      otherDate = otherDate || editing.orthodontic_assessment.date_of_appliance_fitted;
+      otherDate = otherDate || editing.orthodontic_treatment.date_of_completion;
+
+      if(otherDate){
+        var otherMoment = toMomentFilter(otherDate);
+
+        var dobMoment = toMomentFilter(editing.demographics.date_of_birth);
+        var diff = otherMoment.diff(dobMoment, "years", false);
+        if(diff !== 18){
+          return {
+            fp17_exemptions: {
+              aged_18_in_full_time_education: "The patient's DOB was not 18"
+            }
+          }
+        }
+      }
+    }
+  }
+});
---
+++ odonto/static/js/test/fp17o_aged_18_full_time_education.js
@@ -0,0 +1,66 @@
+describe('Fp17OAged18InFullTimeEducation', function() {
+  "use strict";
+  var Fp17OAged18InFullTimeEducation;
+  var editing;
+
+  beforeEach(module('opal.filters'));
+  beforeEach(module('opal.services'));
+
+  beforeEach(function(){
+      inject(function($injector){
+        Fp17OAged18InFullTimeEducation  = $injector.get('Fp17OAged18InFullTimeEducation');
+      });
+      editing = {
+        demographics: {},
+        fp17_exemptions: {},
+        orthodontic_assessment: {},
+        orthodontic_treatment: {}
+      };
+  });
+
+  it('should return an error message if the patient is 19', function(){
+    editing.demographics.date_of_birth = new Date(2000, 1, 1);
+    editing.orthodontic_assessment.date_of_referral = new Date(2019, 1, 2);
+    editing.fp17_exemptions.aged_18_in_full_time_education = true;
+    var expected = {
+      fp17_exemptions: {
+        aged_18_in_full_time_education: "The patient's DOB was not 18"
+      }
+    }
+    expect(Fp17OAged18InFullTimeEducation(editing)).toEqual(expected);
+  });
+
+  it('should return an error message if the patient is 17', function(){
+    editing.demographics.date_of_birth = new Date(2000, 1, 1);
+    editing.orthodontic_assessment.date_of_referral = new Date(2017, 1, 2);
+    editing.fp17_exemptions.aged_18_in_full_time_education = true;
+    var expected = {
+      fp17_exemptions: {
+        aged_18_in_full_time_education: "The patient's DOB was not 18"
+      }
+    }
+    expect(Fp17OAged18InFullTimeEducation(editing)).toEqual(expected);
+  });
+
+  it('should not error if the exemption is not clicked', function(){
+    editing.demographics.date_of_birth = new Date(2000, 1, 1);
+    editing.orthodontic_assessment.date_of_referral = new Date(2019, 1, 2);
+    editing.fp17_exemptions.aged_18_in_full_time_education = false;
+    expect(Fp17OAged18InFullTimeEducation(editing)).toBe(undefined);
+  });
+
+  it('should not error if the patient is 18', function(){
+    editing.demographics.date_of_birth = new Date(2000, 1, 1);
+    editing.orthodontic_assessment.date_of_referral = new Date(2018, 1, 1);
+    editing.fp17_exemptions.aged_18_in_full_time_education = true;
+    expect(Fp17OAged18InFullTimeEducation(editing)).toBe(undefined);
+  });
+
+  it('should prioritise the date of referral', function(){
+    editing.demographics.date_of_birth = new Date(2000, 1, 1);
+    editing.orthodontic_assessment.date_of_referral = new Date(2018, 1, 1);
+    editing.orthodontic_assessment.date_of_assessment = new Date(2033, 1, 1);
+    editing.fp17_exemptions.aged_18_in_full_time_education = true;
+    expect(Fp17OAged18InFullTimeEducation(editing)).toBe(undefined);
+  });
+});
