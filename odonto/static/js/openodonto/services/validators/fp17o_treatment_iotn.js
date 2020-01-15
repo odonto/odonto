33e16715a55b543e54c0d08b10feb36555d96d7f
fredkingham <fredkingham@gmail.com>
2020-01-15 16:52:02 +0000

    Fixes the comment on the treatment iotn validator

--- odonto/static/js/openodonto/services/validators/fp17o_treatment_iotn.js
+++ odonto/static/js/openodonto/services/validators/fp17o_treatment_iotn.js
@@ -1,11 +1,6 @@
 angular.module('opal.services').factory('TreatmentIOTN', function(){
   /*
-  * Checks that the user has entered and IOTN and IOTN NA.
-  *
-  * Also checks that assessment IOTN/IOTN NA has not been set and
-  * differs with treatment iotn
-  * Note the treatment IOTN will make sure we don't enter both an assessment IOTN
-  * and a treatment IOTN/treatment IOTN NA
+  * Checks that the user has not entered an assessment IOTN and a treatment IOTN.
   */
   return function(editing){
     var assessment = editing.orthodontic_assessment;
