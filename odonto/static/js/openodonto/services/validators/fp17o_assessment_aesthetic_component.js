2a7b4052b42eb722ff71f82143699a02b008dc31
fredkingham <fredkingham@gmail.com>
2020-01-30 09:54:26 +0000

    According to a recent email

--- odonto/static/js/openodonto/services/validators/fp17o_assessment_aesthetic_component.js
+++ odonto/static/js/openodonto/services/validators/fp17o_assessment_aesthetic_component.js
@@ -1,15 +1,28 @@
 angular.module('opal.services').factory('Fp17OAssessmentAestheticComponent', function(){
+  "use strict";
   /*
   * An FP17O Assessment IOTN of 3 requires an aesthetic component
+  *
+  * When a patient has an assessment type "Assess & appliance fitted"
+  * an aesthetic component is required.
   */
+
+  var ASSESS_AND_APPLIANCE_FITTED = "Assess & appliance fitted"
   return function(editing){
     var assessment = editing.orthodontic_assessment;
 
-    if(assessment.iotn && assessment.iotn === "3"){
-      if(!assessment.aesthetic_component){
+    if(!assessment.aesthetic_component){
+      if(assessment.iotn && assessment.iotn === "3"){
         return {
           orthodontic_assessment: {
             aesthetic_component: "IOTN 3 requires an aesthetic component"
+          }
+        }
+      }
+      if(assessment.assessment === ASSESS_AND_APPLIANCE_FITTED){
+        return {
+          orthodontic_assessment: {
+            aesthetic_component: "'Assess & appliance fitted' patients require an aesthetic component"
           }
         }
       }
--- odonto/static/js/test/fp17o_assessment_aesthetic_component.test.js
+++ odonto/static/js/test/fp17o_assessment_aesthetic_component.test.js
@@ -2,6 +2,7 @@ describe('Fp17OAssessmentAestheticComponent', function() {
   "use strict";
   var Fp17OAssessmentAestheticComponent;
   var editing;
+  var ASSESS_AND_APPLIANCE_FITTED = "Assess & appliance fitted"
 
   beforeEach(module('opal.services'));
 
@@ -12,7 +13,8 @@ describe('Fp17OAssessmentAestheticComponent', function() {
     editing = {
       orthodontic_assessment: {
         iotn: undefined,
-        aesthetic_component: undefined
+        aesthetic_component: undefined,
+        assessment: undefined
       }
     }
   });
@@ -33,6 +35,25 @@ describe('Fp17OAssessmentAestheticComponent', function() {
 
   it('should not error if IOTN is not 3', function(){
     editing.orthodontic_assessment.iotn = "2";
+    expect(Fp17OAssessmentAestheticComponent(editing)).toBe(undefined);
+  });
+
+  it('should error if assessment is appliance fitted and aesthetic component is not set', function(){
+    editing.orthodontic_assessment.assessment = ASSESS_AND_APPLIANCE_FITTED;
+    var expected = {
+      orthodontic_assessment: {aesthetic_component: "'Assess & appliance fitted' patients require an aesthetic component"}
+    };
+    expect(Fp17OAssessmentAestheticComponent(editing)).toEqual(expected);
+  });
+
+  it('should not error if assessment is something else and aesthetic component is not set', function(){
+    editing.orthodontic_assessment.assessment = "something else";
+    expect(Fp17OAssessmentAestheticComponent(editing)).toBe(undefined);
+  });
+
+  it('should not error assessment is appliance fitted and aesthetic component is set', function(){
+    editing.orthodontic_assessment.assessment = ASSESS_AND_APPLIANCE_FITTED;
+    editing.orthodontic_assessment.aesthetic_component = 2;
     expect(Fp17OAssessmentAestheticComponent(editing)).toBe(undefined);
   });
 });
