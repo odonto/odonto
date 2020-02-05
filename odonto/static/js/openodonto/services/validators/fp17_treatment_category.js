e6560435a10897989f736ef07f6e2e341ddb1c34
fredkingham <fredkingham@gmail.com>
2020-02-05 22:55:52 +0000

    Require a patient charge for reg 11

--- odonto/static/js/openodonto/services/validators/fp17_treatment_category.js
+++ odonto/static/js/openodonto/services/validators/fp17_treatment_category.js
@@ -9,6 +9,11 @@ angular.module('opal.services').factory('Fp17TreatmentCategory', function(){
   *
   * Incomplete treatment must be the same or higher band
   * than the treatment category.
+  *
+  * We believe a charge is required for reg 11
+  * This because we've had a rejection without a charge
+  * described as
+  * "inappropriate patient's charge accompanying Reg 11"
   */
   var BAND_1 = "Band 1";
   var BAND_2 = "Band 2";
@@ -31,7 +36,6 @@ angular.module('opal.services').factory('Fp17TreatmentCategory', function(){
       return getErr("Treatment category is required");
     }
 
-
     if(editing.fp17_other_dental_services.free_repair_or_replacement && treatmentCategory === BAND_1){
       return getErr("A patient cannot have band 1 and free repair or replacement");
     }
@@ -46,6 +50,12 @@ angular.module('opal.services').factory('Fp17TreatmentCategory', function(){
       }
       if(incompleteTreatment == BAND_3 && (treatmentCategory == BAND_1 || treatmentCategory == BAND_2)){
         return getErr("The incomplete treatment band cannot be greater than the treatment category");
+      }
+    }
+
+    if(treatmentCategory === REGULATION_11_REPLACEMENT_APPLIANCE){
+      if(!editing.fp17_exemptions.patient_charge_collected){
+        return getErr('A patient charge is required for reg 11')
       }
     }
   }
