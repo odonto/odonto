de487c6b13d0bceb5867955a5253368dab3a781d
fredkingham <fredkingham@gmail.com>
2020-02-05 22:47:58 +0000

    Slight tidy up of the fp17 treatment category validator (no functional change)

--- odonto/static/js/openodonto/services/validators/fp17_treatment_category.js
+++ odonto/static/js/openodonto/services/validators/fp17_treatment_category.js
@@ -14,50 +14,38 @@ angular.module('opal.services').factory('Fp17TreatmentCategory', function(){
   var BAND_2 = "Band 2";
   var BAND_3 = "Band 3";
   var URGENT_TREATMENT = "Urgent treatment";
+  var REGULATION_11_REPLACEMENT_APPLIANCE = "Regulation 11 replacement appliance";
+
+  var getErr = function(someStr){
+    return {
+      fp17_treatment_category: {
+        treatment_category: someStr
+      }
+    }
+  }
 
   return function(editing){
     var treatmentCategory = editing.fp17_treatment_category.treatment_category;
 
     if(!treatmentCategory|| treatmentCategory === ""){
-      return {
-        fp17_treatment_category: {
-          treatment_category: "Treatment category is required"
-        }
-      }
+      return getErr("Treatment category is required");
     }
 
 
     if(editing.fp17_other_dental_services.free_repair_or_replacement && treatmentCategory === BAND_1){
-      return {
-        fp17_treatment_category: {
-          treatment_category: "A patient cannot have band 1 and free repair or replacement"
-        }
-      }
+      return getErr("A patient cannot have band 1 and free repair or replacement");
     }
     if(editing.fp17_other_dental_services.further_treatment_within_2_months && treatmentCategory === URGENT_TREATMENT){
-      return {
-        fp17_treatment_category: {
-          treatment_category: "A patient cannot have urgent treatment and further treatment within 2 months"
-        }
-      }
+      return getErr("A patient cannot have urgent treatment and further treatment within 2 months");
     }
 
     var incompleteTreatment = editing.fp17_incomplete_treatment.incomplete_treatment
     if(incompleteTreatment){
-      var err = false;
       if(incompleteTreatment == BAND_2 && treatmentCategory == BAND_1){
-        err = true;
+        return getErr("The incomplete treatment band cannot be greater than the treatment category");
       }
       if(incompleteTreatment == BAND_3 && (treatmentCategory == BAND_1 || treatmentCategory == BAND_2)){
-        err = true;
-      }
-
-      if(err){
-        return {
-          fp17_treatment_category: {
-            treatment_category: "The incomplete treatment band cannot be greater than the treatment category"
-          }
-        }
+        return getErr("The incomplete treatment band cannot be greater than the treatment category");
       }
     }
   }
