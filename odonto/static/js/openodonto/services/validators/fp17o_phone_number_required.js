d942400730f295c34e8bf94baf957f280031bb36
fredkingham <fredkingham@gmail.com>
2020-03-30 12:48:58 +0100

    Make mobile number a required field for fp17os

---
+++ odonto/static/js/openodonto/services/validators/fp17o_phone_number_required.js
@@ -0,0 +1,33 @@
+angular.module('opal.services').factory('Fp17OPhoneNumberRequired', function(toMomentFilter){
+  /*
+  * from an email in feb 2020, phone number or phone number declined
+  * are mandatory when submitted in FP17O
+  */
+  return function(editing){
+    "use strict";
+    if(editing.demographics.patient_declined_phone){
+      return;
+    }
+
+    var number = editing.demographics.phone_number;
+
+    if(!number || !number.length){
+      return {
+        demographics: {
+          phone_number: "Mobile number is required"
+        }
+      }
+    }
+
+    var cleanedNumber = number.split("-").join("")
+    cleanedNumber = cleanedNumber.split(" ").join("");
+
+    if(cleanedNumber.length !== 11 || isNaN(cleanedNumber)){
+      return {
+        demographics: {
+          phone_number: "Mobile number is incorrect"
+        }
+      }
+    }
+  }
+});
