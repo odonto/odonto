e778f17656c9389f81c32bdc6fd3d18d22d9a9b4
fredkingham <fredkingham@gmail.com>
2020-05-13 13:01:47 +0100

    Resend all submissions if after a certain date

--- odonto/odonto_submissions/tests/test_send_submissions.py
+++ odonto/odonto_submissions/tests/test_send_submissions.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from django.test import override_settings
 from opal.core.test import OpalTestCase
 from opal.models import Episode
+from odonto.odonto_submissions import models
 from odonto.episode_categories import FP17Episode, FP17OEpisode
 from odonto.odonto_submissions.management.commands import send_submissions
 
@@ -124,10 +125,6 @@ class SendSubmissionGetQSTestCase(OpalTestCase):
         self.fp17o_episode.save()
         self.assertEqual(len(self.cmd.get_fp17os()), False)
 
-    def test_get_fp17oswith_submissions(self):
-        self.fp17o_episode.submission_set.create()
-        self.assertEqual(len(self.cmd.get_fp17os()), False)
-
     def test_get_fp17_qs_success(self):
         self.assertEqual(
             self.cmd.get_fp17_qs().get(),
@@ -150,7 +147,91 @@ class SendSubmissionGetQSTestCase(OpalTestCase):
         self.fp17_episode.save()
         self.assertFalse(self.cmd.get_fp17_qs().exists())
 
-    def test_get_fp17_qs_with_submissions(self):
-        self.fp17_episode.submission_set.create()
-        self.assertFalse(self.cmd.get_fp17_qs().exists())
 
+class FilterForNewOrFailedSinceTestCase(OpalTestCase):
+    def setUp(self):
+        self.cmd = send_submissions.Command()
+        self.patient, self.fp17_episode = self.new_patient_and_episode_please()
+        self.this_year = datetime.date(2020, 4, 1)
+        self.last_year = datetime.date(2019, 4, 1)
+
+        # an fp17 episode ready to be submitted
+        self.fp17_episode.stage = FP17Episode.SUBMITTED
+        self.fp17_episode.category_name = FP17Episode.display_name
+        self.fp17_episode.save()
+
+    def test_return_failed_this_tax_year(self):
+        self.fp17_episode.fp17incompletetreatment_set.update(
+            completion_or_last_visit=self.this_year
+        )
+        self.fp17_episode.submission_set.create(
+            state=models.Submission.REJECTED_BY_COMPASS
+        )
+        result = self.cmd.filter_for_new_or_failed_since(
+            Episode.objects.all()
+        )
+        self.assertEqual(
+            result, [self.fp17_episode]
+        )
+
+    def test_return_episodes_with_no_submissions(self):
+        self.fp17_episode.fp17incompletetreatment_set.update(
+            completion_or_last_visit=self.this_year
+        )
+        result = self.cmd.filter_for_new_or_failed_since(
+            Episode.objects.all()
+        )
+        self.assertEqual(
+            result, [self.fp17_episode]
+        )
+
+    def test_do_not_return_old_episodes(self):
+        self.fp17_episode.fp17incompletetreatment_set.update(
+            completion_or_last_visit=self.last_year
+        )
+        self.fp17_episode.submission_set.create(
+            state=models.Submission.REJECTED_BY_COMPASS
+        )
+        result = self.cmd.filter_for_new_or_failed_since(
+            Episode.objects.all()
+        )
+        self.assertEqual(
+            result, []
+        )
+
+    def test_do_not_return_episodes_already_succeeded(self):
+        self.fp17_episode.fp17incompletetreatment_set.update(
+            completion_or_last_visit=self.this_year
+        )
+        self.fp17_episode.submission_set.create(
+            state=models.Submission.SUCCESS
+        )
+        result = self.cmd.filter_for_new_or_failed_since(
+            Episode.objects.all()
+        )
+        self.assertEqual(
+            result, []
+        )
+
+    def test_with_fp17o(self):
+        fp17o_episode = self.patient.create_episode()
+        fp17o_episode.stage = FP17OEpisode.SUBMITTED
+        fp17o_episode.category_name = FP17OEpisode.display_name
+        fp17o_episode.save()
+        fp17o_episode.orthodonticassessment_set.update(
+            date_of_assessment=self.this_year
+        )
+        result = self.cmd.filter_for_new_or_failed_since(
+            Episode.objects.filter(category_name=FP17OEpisode.display_name)
+        )
+        self.assertEqual(
+            result, [fp17o_episode]
+        )
+
+    def test_with_none(self):
+        result = self.cmd.filter_for_new_or_failed_since(
+            Episode.objects.none()
+        )
+        self.assertEqual(
+            result, []
+        )
