19c63f2c49f310c5dcb6f6bbec82e3e67ac9c756
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-12-08 19:37:22 +0000

    Location should be a number and dpb site id should be a string

--- odonto/settings.py
+++ odonto/settings.py
@@ -337,7 +337,7 @@ ODONTO_LOGIN_MESSAGE = 'Log in to get started'
 # <---- DPB Credentials
 DPB_USERNAME = None
 DPB_PASSWORD = None
-DPB_SITE_ID = 89651
+DPB_SITE_ID = "89651"
 
 SEND_MESSAGES = False
 LOCATION = 10108
