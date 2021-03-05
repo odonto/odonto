421a1c8d03959697e1dd0a634d3e3ded02dc3017
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-03-05 16:03:17 +0000

    Adds in the covid triage models

--- odonto/models.py
+++ odonto/models.py
@@ -846,3 +846,110 @@ class CaseMix(models.EpisodeSubrecord):
         if total < 30:
             return self.SEVERE_COMPLEXITY
         return self.EXTREME_COMPLEXITY
+
+
+class CovidTriage(models.EpisodeSubrecord):
+    """
+    Triage models are the data used by the covid
+    triage episode category.
+
+    They store data about patient interaction
+    where there is not going to be an FP17 or FP17O
+    """
+    class Meta:
+        verbose_name = "Covid triage"
+    
+    # Order is important for the choices
+    # the code sent to compass is the idx + 1
+    LOCAL_UCD_REFERRAL_REASONS = enum(
+        "Life threatening emergencies",
+        "Trauma",
+        "Oro-facial swelling",
+        "Post extraction bleeding",
+        "Dental conditions resulting in systematic illness",
+        "Severe dental or facial pain",
+        "Fractured teeth with pulp exposure",
+        "Dental and soft tissue infections",
+        "Suspected oral cancer",
+        "Oro-dental conditions worsening systemic illness"
+    )
+
+    COVID_STATUS = enum(
+        "Patient is shielded",
+        "Increased risk of illness from COVID-19",
+        "Possible/confirmed COVID-19 patient (or those living in household)",
+        "Other",
+        "Patient is COVID-19 symptom free at present"
+    )
+
+    REASONS_FOR_THE_CALL = enum(
+        "Pain",
+        "Swelling",
+        "Bleeding"
+        "Trauma",
+        "Soft tissue pathology",
+        "Other",
+        "Routine treatment"
+    )
+    date_of_contact = fields.DateField(
+        verbose_name="Date of contact"
+    )
+    time_of_contact = fields.TimeField(
+        verbose_name="Time the call ended"
+    )
+    data_care_professional = fields.BooleanField(
+        default=False, 
+        verbose_name="Carried out by a dental care professional"
+    )
+    primary_reason = fields.CharField(
+        blank=True, 
+        null=True, 
+        max_length=256,
+        verbose_name='Primary reason for call',
+        choices=REASONS_FOR_THE_CALL
+    )
+    covid_status = fields.CharField(
+        blank=True, 
+        null=True, 
+        max_length=256, 
+        verbose_name="Covid status",
+        choices=COVID_STATUS
+    )
+    triage_via_video = fields.BooleanField(
+        default=False,
+        verbose_name="Triage via video"
+    )
+    advice_given = fields.BooleanField(
+        default=False, verbose_name="Advice given"
+    )
+    advised_analgesics = fields.BooleanField(
+        default=False, verbose_name="Advised analgesics"
+    )
+    remote_prescription_analgesics = fields.BooleanField(
+        default=False, 
+        verbose_name="Remote prescription of analgesics"
+    )
+    remote_prescription_antibiotics = fields.BooleanField(
+        default=False, 
+        verbose_name="Remote prescription of antibiotics"
+    )
+    follow_up_call_required = fields.BooleanField(
+        default=False,
+        verbose_name="Follow up call required"
+    )
+    call_back_if_symptoms_worsen = fields.BooleanField(
+        default=False,
+        verbose_name="Recommended a call back if symptoms worsened"
+    )
+    referrered_to_local_udc_reason = fields.CharField(
+        blank=True,
+        null=True,
+        max_length=256,
+        verbose_name="Reason the patient has been referred to the local UDC"
+    )
+    face_to_face_appointment = fields.BooleanField(
+        default=False,
+        verbose_name="The triage call recommended a face to face but patient failed to attend"
+    )
+
+
