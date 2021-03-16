3f6bbf1d77b38998a0c41ea14be3936b8eafa762
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-03-16 11:17:28 +0000

    Adds a comment string to the covid triage step

--- odonto/static/js/openodonto/controllers/covid_triage.step.controller.js
+++ odonto/static/js/openodonto/controllers/covid_triage.step.controller.js
@@ -1,5 +1,15 @@
 angular.module('opal.controllers').controller('CovidTriageStepCtrl', function(scope, step, episode){
   "use strict";
+  /*
+  * This controller does 2 things.
+  * 1. The form input for time returns a javascript date object,
+  *    the server wants a string in the form HH:MM:SS, so we change it onload from string to date
+  *    and back on save.
+  * 2. The form has a check box for if the user is referred to urgent dental care. This is client
+  *    only as the covid triage compass spec only requries a reason. The conrollter checks the checkbox
+  *    if there is a reason.
+  */
+
 
   var setUp = function(){
     scope.local = {referred: false};
