17dcc0e8a307495c65e0236ae7a738e58e6cb081
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-06-28 18:37:48 +0100

    default the datetime of triage to now

--- odonto/static/js/openodonto/controllers/covid_triage.step.controller.js
+++ odonto/static/js/openodonto/controllers/covid_triage.step.controller.js
@@ -2,9 +2,7 @@ angular.module('opal.controllers').controller('CovidTriageStepCtrl', function(sc
   "use strict";
   /*
   * This controller does 2 things.
-  * 1. The form input for time returns a javascript date object,
-  *    the server wants a string in the form HH:MM:SS, so onload we create a local variable as a date
-  *    when this local variable is updated we change that back into a HH:MM:SS on the editing variable
+  * 1. Defaults the datetime of contact to now if its now already set.
   * 2. The form has a check box for if the user is referred to urgent dental care. This is client
   *    only as the covid triage compass spec only requries a reason. The conrollter checks the checkbox
   *    if there is a reason.
@@ -15,6 +13,9 @@ angular.module('opal.controllers').controller('CovidTriageStepCtrl', function(sc
     scope.local = {referred: false};
     if(scope.editing.covid_triage.referrered_to_local_udc_reason){
       scope.local.referred = true;
+    }
+    if(!scope.editing.covid_triage.datetime_of_contact){
+      scope.editing.covid_triage.datetime_of_contact = new Date();
     }
   }
 
