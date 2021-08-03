743b56d7eb0cff561fba72578df4489e7d6d2ab4
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-08-03 10:13:23 +0100

    Bumping the version number to 0.45

--- odonto/settings.py
+++ odonto/settings.py
@@ -318,7 +318,7 @@ DEFAULT_DOMAIN = 'http://openodonto.com/'
 # Begins OPAL Settings you should edit !
 
 OPAL_BRAND_NAME = 'Open Odonto'
-VERSION_NUMBER = '0.43.0'
+VERSION_NUMBER = '0.45.0'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
