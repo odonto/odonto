a6315bb7d570d8cc06fca7acee35547187a04377
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-09-09 15:31:42 +0100

    fixes the unit tests to work sending reports

--- odonto/odonto_submissions/tests/test_send_report.py
+++ odonto/odonto_submissions/tests/test_send_report.py
@@ -1,21 +1,21 @@
 import datetime
 from opal.core.test import OpalTestCase
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from opal.models import Episode
 from odonto.odonto_submissions.models import Submission, EpisodesBeingInvestigated
 from odonto.episode_categories import FP17Episode
-from odonto.odonto_submissions.management.commands import get_responses
+from odonto.odonto_submissions.management.commands import send_report
 
-BASE_STR = "odonto.odonto_submissions.management.commands.get_responses"
+BASE_STR = "odonto.odonto_submissions.management.commands.send_report"
 
 
 @patch(BASE_STR + ".send_mail")
-@patch(BASE_STR + ".Response.get")
 @patch(BASE_STR + ".render_to_string")
 @patch(BASE_STR + ".logger")
 class GetResponsesTestCase(OpalTestCase):
     def setUp(self):
-        self.cmd = get_responses.Command()
+        self.cmd = send_report.Command()
+        self.response = MagicMock()
 
     def get_episode(self):
         _, episode = self.new_patient_and_episode_please()
@@ -37,16 +37,10 @@ translate_episode_to_xml"
         submission.save()
         return submission
 
-    def test_with_error(self, logger, render_to_string, response_get, send_mail):
-        response_get.side_effect = ValueError("boom")
-        self.cmd.handle()
-        logger.error.assert_called_once_with("Failed to get responses")
-
-    def test_without_none(self, logger, render_to_string, response_get, send_mail):
-        response = response_get.return_value
-        response.get_successfull_submissions.return_value = Submission.objects.all()
-        response.get_rejected_submissions.return_value = {}
-        self.cmd.handle()
+    def test_without_none(self, logger, render_to_string, send_mail):
+        self.response.get_successfull_submissions.return_value = Submission.objects.all()
+        self.response.get_rejected_submissions.return_value = {}
+        self.cmd.send_email(self.response)
         ctx = render_to_string.call_args[0][1]
         expected_summary = {
             "FP17 Success": 0,
@@ -57,13 +51,12 @@ translate_episode_to_xml"
         self.assertEqual(ctx["summary"]["Latest response"], expected_summary)
         self.assertTrue(ctx["title"].startswith("Odonto response information for"))
 
-    def test_with_success(self, logger, render_to_string, response_get, send_mail):
+    def test_with_success(self, logger, render_to_string, send_mail):
         episode = self.get_episode()
         self.get_submission(episode, Submission.SUCCESS)
-        response = response_get.return_value
-        response.get_successfull_submissions.return_value = Submission.objects.all()
-        response.get_rejected_submissions.return_value = {}
-        self.cmd.handle()
+        self.response.get_successfull_submissions.return_value = Submission.objects.all()
+        self.response.get_rejected_submissions.return_value = {}
+        self.cmd.send_email(self.response)
         ctx = render_to_string.call_args[0][1]
         expected_summary = {
             "FP17 Success": 1,
@@ -105,15 +98,14 @@ translate_episode_to_xml"
         self.assertTrue(ctx["title"].startswith("Odonto response information for"))
         self.assertFalse(ctx["title"].endswith("NEEDS INVESTIGATION"))
 
-    def test_with_rejection(self, logger, render_to_string, response_get, send_mail):
+    def test_with_rejection(self, logger, render_to_string, send_mail):
         episode = self.get_episode()
         self.get_submission(episode, Submission.REJECTED_BY_COMPASS)
-        response = response_get.return_value
-        response.get_successfull_submissions.return_value = Submission.objects.none()
-        response.get_rejected_submissions.return_value = {
+        self.response.get_successfull_submissions.return_value = Submission.objects.none()
+        self.response.get_rejected_submissions.return_value = {
             Submission.objects.get(): "fail"
         }
-        self.cmd.handle()
+        self.cmd.send_email(self.response)
         ctx = render_to_string.call_args[0][1]
         expected_summary = {
             "FP17 Success": 0,
@@ -126,7 +118,7 @@ translate_episode_to_xml"
         expected_current_fp17 = {
             'Oldest unsent': datetime.date.today(),
             'Open': 0,
-            'Rejected by compass': get_responses.WarningField(1)
+            'Rejected by compass': send_report.WarningField(1)
         }
         self.assertEqual(
             ctx["summary"]["FP17 current tax year"],
@@ -159,19 +151,19 @@ translate_episode_to_xml"
         self.assertTrue(ctx["title"].startswith("Odonto response information for"))
         self.assertTrue(ctx["title"].endswith("NEEDS INVESTIGATION"))
 
-    def test_clean_episodes_episode_succeeded(self, logger, render_to_string, response_get, send_mail):
+    def test_clean_episodes_episode_succeeded(self, logger, render_to_string, send_mail):
         episode = self.get_episode()
         self.get_submission(episode, Submission.SUCCESS)
         episode.episodesbeinginvestigated_set.create()
-        get_responses.clean_episodes_being_investigated()
+        send_report.clean_episodes_being_investigated()
         self.assertEqual(Episode.objects.get().id, episode.id)
         self.assertFalse(EpisodesBeingInvestigated.objects.exists())
 
-    def test_clean_episodes_episode_failed(self, logger, render_to_string, response_get, send_mail):
+    def test_clean_episodes_episode_failed(self, logger, render_to_string, send_mail):
         episode = self.get_episode()
         self.get_submission(episode, Submission.REJECTED_BY_COMPASS)
         episode.episodesbeinginvestigated_set.create()
-        get_responses.clean_episodes_being_investigated()
+        send_report.clean_episodes_being_investigated()
         self.assertEqual(Episode.objects.get().id, episode.id)
         self.assertEqual(
             EpisodesBeingInvestigated.objects.get().episode_id, episode.id
