ea0415ec3e88e8efebfbbf2ce42efacd646f1478
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-06-07 09:39:43 +0100

    remove the previous year from the stats page as this is now part of the navigation

--- odonto/views.py
+++ odonto/views.py
@@ -247,11 +247,6 @@ class Stats(LoginRequiredMixin, TemplateView):
         year = int(self.kwargs["year"])
         return self.get_range_for_year(year)
 
-    @cached_property
-    def previous_date_range(self):
-        year = int(self.kwargs["year"]) - 1
-        return self.get_range_for_year(year)
-
     def get_fp17_qs(self, date_range):
         return Episode.objects.filter(
             category_name=episode_categories.FP17Episode.display_name
@@ -296,22 +291,14 @@ class Stats(LoginRequiredMixin, TemplateView):
             yield date_range
 
     def get_month_totals(self):
-        result = {}
         monthly_claims = []
-        time_periods = {
-            "current": self.date_range,
-            "previous": self.previous_date_range
-        }
-        for period_name, period_range in time_periods.items():
-            monthly_claims = []
-            for date_range in self.month_iterator(period_range[0]):
-                successful_fp17o_count = self.get_successful_fp17os(date_range).count()
-                successful_fp17_count = self.get_successful_fp17s(date_range).count()
-                monthly_claims.append(
-                    successful_fp17o_count + successful_fp17_count
-                )
-            result[period_name] = monthly_claims
-        return result
+        for date_range in self.month_iterator(self.date_range[0]):
+            successful_fp17o_count = self.get_successful_fp17os(date_range).count()
+            successful_fp17_count = self.get_successful_fp17s(date_range).count()
+            monthly_claims.append(
+                successful_fp17o_count + successful_fp17_count
+            )
+        return monthly_claims
 
     def get_state_counts(self):
         return {
@@ -332,62 +319,50 @@ class Stats(LoginRequiredMixin, TemplateView):
         }
 
     def get_uoa_data(self):
-        time_periods = {
-            "current": self.date_range,
-            "previous": self.previous_date_range
-        }
-        by_period = defaultdict(list)
+        by_period = []
         by_performer = defaultdict(int)
 
-        for period_name, period_range in time_periods.items():
-            for date_range in self.month_iterator(period_range[0]):
-                fp17os = self.get_successful_fp17os(date_range)
-                fp17os = fp17os.prefetch_related(
-                    'orthodonticassessment_set',
-                    'orthodontictreatment_set',
-                    'fp17dentalcareprovider_set',
-                )
-                month_uoa_total = 0
-                for fp17o in fp17os:
-                    uoa = fp17o.category.uoa()
-                    if not uoa:
-                        continue
+        for date_range in self.month_iterator(self.date_range[0]):
+            fp17os = self.get_successful_fp17os(date_range)
+            fp17os = fp17os.prefetch_related(
+                'orthodonticassessment_set',
+                'orthodontictreatment_set',
+                'fp17dentalcareprovider_set',
+            )
+            month_uoa_total = 0
+            for fp17o in fp17os:
+                uoa = fp17o.category.uoa()
+                if not uoa:
+                    continue
 
-                    month_uoa_total += uoa
-                    if period_name == "current":
-                        performer = fp17o.fp17dentalcareprovider_set.all()[0].performer
-                        by_performer[performer] += uoa
-                by_period[period_name].append(month_uoa_total)
+                month_uoa_total += uoa
+                performer = fp17o.fp17dentalcareprovider_set.all()[0].performer
+                by_performer[performer] += uoa
+            by_period.append(month_uoa_total)
         return by_period, by_performer
 
     def get_uda_data(self):
-        time_periods = {
-            "current": self.date_range,
-            "previous": self.previous_date_range
-        }
-        by_period = defaultdict(list)
+        by_period = []
         by_performer = defaultdict(lambda: defaultdict(int))
 
-        for period_name, period_range in time_periods.items():
-            for date_range in self.month_iterator(period_range[0]):
-                fp17s = self.get_successful_fp17s(date_range)
-                fp17s = fp17s.prefetch_related(
-                    'fp17treatmentcategory_set',
-                    'fp17dentalcareprovider_set'
-                )
-                month_uda_total = 0
-                for fp17 in fp17s:
-                    uda = fp17.category.uda()
-                    if not uda:
-                        continue
+        for date_range in self.month_iterator(self.date_range[0]):
+            fp17s = self.get_successful_fp17s(date_range)
+            fp17s = fp17s.prefetch_related(
+                'fp17treatmentcategory_set',
+                'fp17dentalcareprovider_set'
+            )
+            month_uda_total = 0
+            for fp17 in fp17s:
+                uda = fp17.category.uda()
+                if not uda:
+                    continue
 
-                    month_uda_total += uda
-                    if period_name == "current":
-                        treatment = fp17.fp17treatmentcategory_set.all()[0]
-                        performer = fp17.fp17dentalcareprovider_set.all()[0].performer
-                        by_performer[performer]["uda"] += uda
-                        by_performer[performer][treatment.treatment_category] += 1
-                by_period[period_name].append(round(month_uda_total))
+                month_uda_total += uda
+                treatment = fp17.fp17treatmentcategory_set.all()[0]
+                performer = fp17.fp17dentalcareprovider_set.all()[0].performer
+                by_performer[performer]["uda"] += uda
+                by_performer[performer][treatment.treatment_category] += 1
+            by_period.append(round(month_uda_total))
         return by_period, by_performer
 
     def aggregate_performer_information(self, uda_by_performer, uoa_by_performer):
@@ -422,22 +397,20 @@ class Stats(LoginRequiredMixin, TemplateView):
             uda_by_performer, uoa_by_performer
         )
         current = f"{self.date_range[0].year}-{self.date_range[1].year}"
-        previous = f"{self.previous_date_range[0].year}-{self.previous_date_range[1].year}"
         today = datetime.date.today()
         menu_years = sorted([today.year - i for i in range(4)])
 
         ctx.update({
             "menu_years": menu_years,
             "current": current,
-            "previous": previous,
             "state_counts": self.get_state_counts(),
             "month_totals": json.dumps(self.get_month_totals()),
             "uda_info": {
-                "total":  sum(uda_by_period["current"]),
+                "total":  sum(uda_by_period),
                 "by_period": json.dumps(uda_by_period)
             },
             "uoa_info": {
-                "total":  sum(uoa_by_period["current"]),
+                "total":  sum(uoa_by_period),
                 "by_period": json.dumps(uoa_by_period)
             },
             "performer_info": performer_info
