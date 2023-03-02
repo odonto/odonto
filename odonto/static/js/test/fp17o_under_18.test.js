e9768ed6ff054d95d5caabc5f60a4460613ebb1b
fredkingham <fred.kingham@openhealthcare.org.uk>
2023-03-02 15:45:02 +0000

    We should not use date of referral in fp17O under 18 checks
    This should only be used pre April 2019

--- odonto/static/js/test/fp17o_under_18.test.js
+++ odonto/static/js/test/fp17o_under_18.test.js
@@ -22,7 +22,7 @@ describe('Fp17oUnder18', function() {
   describe('if a patient has the under 18 exemption it should error if their dob is older', function(){
     it('should return an error message if dob is to recent', function(){
       editing.demographics.date_of_birth = new Date(1980, 1, 1);
-      editing.orthodontic_assessment.date_of_referral = new Date(2019, 1, 1);
+      editing.orthodontic_assessment.date_of_assessment = new Date(2019, 1, 1);
       editing.fp17_exemptions.patient_under_18 = true;
       var expected = {
         fp17_exemptions: {
@@ -34,14 +34,14 @@ describe('Fp17oUnder18', function() {
 
     it('should not error if the exemption is not clicked and the patient is not under 18', function(){
       editing.demographics.date_of_birth = new Date(1980, 1, 1);
-      editing.orthodontic_assessment.date_of_referral = new Date(2019, 1, 1);
+      editing.orthodontic_assessment.date_of_assessment = new Date(2019, 1, 1);
       editing.fp17_exemptions.patient_under_18 = false;
       expect(Fp17oUnder18(editing)).toBe(undefined);
     });
 
     it('should error if the exemption is not clicked and the patient is under 18', function(){
       editing.demographics.date_of_birth = new Date(2000, 1, 1);
-      editing.orthodontic_assessment.date_of_referral = new Date(2017, 1, 1);
+      editing.orthodontic_assessment.date_of_assessment = new Date(2017, 1, 1);
       editing.fp17_exemptions.patient_under_18 = false;
       var expected = {
         fp17_exemptions: {
@@ -53,7 +53,7 @@ describe('Fp17oUnder18', function() {
 
     it('should not error if the exemption is not clicked and the patient is under 18 if it is a completion claim', function(){
       editing.demographics.date_of_birth = new Date(20160, 1, 1);
-      editing.orthodontic_assessment.date_of_referral = new Date(2019, 1, 1);
+      editing.orthodontic_assessment.date_of_assessment = new Date(2019, 1, 1);
       editing.orthodontic_treatment.completion_type = "Treatment completed";
       editing.fp17_exemptions.patient_under_18 = false;
       expect(Fp17oUnder18(editing)).toBe(undefined);
@@ -61,14 +61,14 @@ describe('Fp17oUnder18', function() {
 
     it('should not error if the date of birth is under 18 years ago', function(){
       editing.demographics.date_of_birth = new Date(2015, 1, 1);
-      editing.orthodontic_assessment.date_of_referral = new Date(2019, 1, 1);
+      editing.orthodontic_assessment.date_of_assessment = new Date(2019, 1, 1);
       editing.fp17_exemptions.patient_under_18 = true;
       expect(Fp17oUnder18(editing)).toBe(undefined);
     });
 
     it('should error if the patient is just 18', function(){
       editing.demographics.date_of_birth = new Date(2002, 10, 1);
-      editing.orthodontic_assessment.date_of_referral =  new Date(2020, 10, 1);
+      editing.orthodontic_assessment.date_of_assessment =  new Date(2020, 10, 1);
       editing.fp17_exemptions.patient_under_18 = true;
       var expected = {
         fp17_exemptions: {
@@ -80,17 +80,22 @@ describe('Fp17oUnder18', function() {
 
     it('should not error if the patient is nearly 18', function(){
       editing.demographics.date_of_birth = new Date(2002, 10, 10);
-      editing.orthodontic_assessment.date_of_referral = new Date(2020, 10, 9);
+      editing.orthodontic_assessment.date_of_assessment = new Date(2020, 10, 9);
       editing.fp17_exemptions.patient_under_18 = true;
       expect(Fp17oUnder18(editing)).toBe(undefined);
     })
 
-    it('should prioritise the date of referral', function(){
+    it('should not use the date of referral', function(){
       editing.demographics.date_of_birth = new Date(2003, 1, 1);
       editing.orthodontic_assessment.date_of_referral = new Date(2018, 1, 1);
       editing.orthodontic_assessment.date_of_assessment = new Date(2033, 1, 1);
       editing.fp17_exemptions.patient_under_18 = true;
-      expect(Fp17oUnder18(editing)).toBe(undefined);
+      var expected = {
+        fp17_exemptions: {
+          patient_under_18: "This patient is not under 18"
+        }
+      }
+      expect(Fp17oUnder18(editing)).toEqual(expected);
     });
   });
 });
